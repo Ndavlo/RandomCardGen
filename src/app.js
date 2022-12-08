30a95327f9e3aed83a05f52fed5241b2714afe6b
Ndavlo <nicolasdl99@gmail.com>
2022-12-08 02:26:53 +0000

    Card Gen

--- src/app.js
+++ src/app.js
@@ -1,11 +1,42 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  newCard();
 };
+
+document.getElementById("button").addEventListener("click", newCard);
+
+function newCard() {
+  let figures = ["♠", "♣", "♥", "♦"];
+  let random = Math.floor(Math.random() * 4);
+  let valor = Math.floor(Math.random() * 13) + 1;
+  switch (valor) {
+    case 1:
+      valor = "A";
+      break;
+    case 11:
+      valor = "J";
+      break;
+    case 12:
+      valor = "Q";
+      break;
+    case 13:
+      valor = "K";
+      break;
+  }
+  let figurasDOM = document.getElementsByClassName("icon");
+  for (let i = 0; i < figurasDOM.length; i++) {
+    figurasDOM.item(i).innerHTML = figures[random];
+    if (random > 1) {
+      figurasDOM.item(i).classList.add("red");
+    } else {
+      figurasDOM.item(i).classList.remove("red");
+    }
+  }
+  let valorDOM = document.querySelector(".number");
+  valorDOM.innerHTML = valor;
+  if (random > 1) {
+    valorDOM.classList.add("red");
+  } else {
+    valorDOM.classList.remove("red");
+  }
+}
